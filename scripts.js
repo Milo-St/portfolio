b0a45d5938f288858aa73eff67e2d23648ababd0
Milo-St <140327@student.drenthecollege.nl>
2024-05-09 14:38:39 +0200

    added navbar functionality

---
+++ scripts.js
@@ -0,0 +1,35 @@
+function scrollToHome() {
+    $('html, body').animate({
+        scrollTop: $("#home-container").offset().top
+    }, 1000); 
+}
+
+function scrollToContact() {
+    $('html, body').animate({
+        scrollTop: $("#contact-container").offset().top
+    }, 1000);
+}
+
+function scrollToTestimonials() {
+    $('html, body').animate({
+        scrollTop: $("#testimonials-container").offset().top
+    }, 1000);
+}
+
+function scrollToProjects() {
+    $('html, body').animate({
+        scrollTop: $("#projects-container").offset().top
+    }, 1000);
+}
+
+function scrollToServices() {
+    $('html, body').animate({
+        scrollTop: $("#services-title").offset().top
+    }, 1000);
+}
+
+function scrollToAboutme() {
+    $('html, body').animate({
+        scrollTop: $("#about-me-container").offset().top
+    }, 1000);
+}
