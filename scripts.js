57ff9d206e5da39df0c43db0235c66ddd4298444
Milo-St <140327@student.drenthecollege.nl>
2024-05-12 22:55:38 +0200

    added projects images and functionality to cycle between them
    this took me a very long time, I am well aware that there are some bugs in this including the image scaling. I sadly don't have enough time as of right now to fix these. I will try to fix them whenever I have time later

--- scripts.js
+++ scripts.js
@@ -33,3 +33,40 @@ function scrollToAboutme() {
         scrollTop: $("#about-me-container").offset().top
     }, 1000);
 }
+
+
+var currentImage = 0;
+
+function changeImage(direction) {
+
+    if (direction === 'next') {
+        currentImage += 1;
+    } else if (direction === 'prev') {
+        currentImage -= 1;
+    }
+
+     if(currentImage === -1) {
+        currentImage = 0;
+    } else if (currentImage === 0) {
+        document.getElementById("current-project").innerHTML = "<h5>G</h5><h1>ame library</h1>";
+        document.getElementById("projects-image1").innerHTML = "<img src='images/image1.jpg' id='responsive-image'>";
+        document.getElementById("projects-image2").innerHTML = "<img src='images/image2.jpg' id='responsive-image'>";
+        document.getElementById("projects-image3").innerHTML = "<img src='images/image3.jpg' id='responsive-image'>";
+    } else if(currentImage === 1) {
+        document.getElementById("current-project").innerHTML = "<h5>B</h5><h1>attle ships</h1>";
+        document.getElementById("projects-image1").innerHTML = "<img src='images/image4.jpg' id='responsive-image'>";
+        document.getElementById("projects-image2").innerHTML = "<img src='images/image5.jpg' id='responsive-image'>";
+        document.getElementById("projects-image3").innerHTML = "<img src='images/image6.jpg' id='responsive-image'>";
+    } else if(currentImage === 2) {
+        document.getElementById("current-project").innerHTML = "<h5>P</h5><h1>okemon API</h1>";
+        document.getElementById("projects-image1").innerHTML = "<img src='images/image7.jpg' id='responsive-image'>";
+        document.getElementById("projects-image2").innerHTML = "<img src='images/image8.jpg' id='responsive-image'>";
+        document.getElementById("projects-image3").innerHTML = "<img src='images/image9.jpg' id='responsive-image'>";
+    } else if(currentImage === 3) {
+        document.getElementById("current-project").innerHTML = "<h5>M</h5><h1>astermind</h1>";
+        document.getElementById("projects-image1").innerHTML = "<img src='images/image10.jpg' id='responsive-image'>";
+        document.getElementById("projects-image2").innerHTML = "<img src='images/image11.jpg' id='responsive-image'>";
+        document.getElementById("projects-image3").innerHTML = "<img src='images/image12.jpg' id='responsive-image'>";
+        currentImage = -1;
+    }
+}
